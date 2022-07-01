65eab98c3d01afe4f1f058558caff3a836e90740
Paulius Uliackas <47225635+PauliusUliackas@users.noreply.github.com>
2022-07-01 20:12:01 +0300

    Veikianti versija 4 Writer klases

--- index.php
+++ index.php
@@ -7,15 +7,12 @@ Issaugoti duomenis is masyvo, i faila (CSV, JSON, XML)
 Reikalavimai:
 KISS funkcija turi buti maziau negu 50 liniju kodo.
 DRY negalima kopijuoti arba rasyti identisko kodo.
-SOLID Kiekviena class turi buti naudojama vienai reksmei 
-(Planas naudoti interface CSV JSON ir XML atskirai).
+SOLID Kiekviena class turi buti naudojama vienai reksmei
 
 
 Uzduociai atlikti buvo naudojamas PHP su XAMPP pagalba.
 
 */
-
-echo " DEBUG";
 
 $data = array(array('first_name'=>'Kiestis', 'age'=>'29', 'gender'=>'male'),
 array('first_name'=>'Vytska', 'age'=>'32', 'gender'=>'male'),
@@ -36,28 +33,36 @@ class Person
     private $age;
     private $gender;
 
-    function __construct($name, $age, $gender)
+    public function __construct($name, $age, $gender)
     {
         $this->name = $name;
         $this->age = $age;
         $this->gender = $gender;
     }
 
-    function __destruct(){}
+    public function __destruct()
+    {
 
-    function getName()
+    }
+
+    public function getName()
     {
         return $this->name;
     }
 
-    function getAge()
+    public function getAge()
     {
         return $this->age;
     }
 
-    function getGender()
+    public function getGender()
     {
         return $this->gender;
+    }
+
+    public function asArray()
+    {
+        return array('first_name'=>$this->name, 'age'=>$this->age, 'gender'=>$this->gender);
     }
 
 }
@@ -67,7 +72,8 @@ class Person
 class Reader skirta doumenu skaitymui is masyvo.
 constructor sukuria nauja masyva atminciai. 
 Galima perskaityti kelis skirtingus masyvus ir tada viska irasyti vienu kartu.
-Taip pat galima pasiekti atminti jeigu del kazkokiu atveju reiktu kazka irtrinti ar pakeist.
+Taip pat galima pasiekti atminti,
+jeigu del kazkokiu atveju reiktu kazka istrinti ar pakeist.
 
 */
 
@@ -76,52 +82,122 @@ class Reader
      // atmintis
     private $data;
 
-    function __construct() {
+    public function __construct() 
+    {
         $data = array();
     }
 
-    function __destruct(){}
+    public function __destruct()
+    {}
 
     // Skaito is skirto masyvo -> data.
-    function read($data){
-        for($i = 0; $i < count($data); $i++){  
-            $this->data[] = new Person($data[0], $data[1], $data[2]);
+    public function read($data)
+    {
+        for($i = 0; $i < count($data); $i++){ 
+            $this->data[] = new Person($data[$i]['first_name'], $data[$i]['age'], $data[$i]['gender']);
         }
     }
 
     // Galima pasiekti atminti.
-    function getData(){
-        return $data;
+    public function getData()
+    {
+        return $this->data;
     }
 
     // funkcija skirta isvalyti visa atminti
-    function clear(){
+    public function clear()
+    {
         $this->data = array();
-    }
-
-};
-
-class Writer
-{
-
-    function __construct(){}
-
-    function __destruct(){}
-
-    function writeToJson($data){
-        
-    }
-
-    function writeToCSV($data){
-
-    }
-
-    function writeToXML($data){
-
     }
 
 }
 
+abstract class Writer
+{
+    abstract protected function process($data);
+
+    public function write($filename, $info)
+    {
+        $file = fopen($filename, "w");
+        fwrite($file, $info);
+        fclose($file);
+    }
+}
+// Poorly pu to files
+class WriterJSON extends Writer
+{
+    public function process($data)
+    {
+        $file = fopen("database.json", "w");
+        $output = array();
+        for($index = 0; $index < count($data); $index++){
+            $output[] = $data[$index]->asArray();
+        }
+        fwrite($file, json_encode($output));
+        fclose($file);
+    }
+}
+
+class WriterCSV extends Writer
+{
+    public function process($data)
+    {
+        $header = array();
+        $file = fopen("database.csv", "w");
+        for($index = 0; $index < count($data); $index++){
+            $info = array();
+            $person = $data[$index]->asArray();
+            foreach($data[$index]->asArray() as $key => $value){
+                if($index == 0){
+                    $header[] = $key;
+                }
+                $info[] = $value;
+            }
+            echo var_dump($person) . "<br>";
+            if($index == 0){
+                fputcsv($file, $header);
+            }
+            fputcsv($file, $info);
+        }
+        fclose($file);
+    }
+}
+
+class WriterXML extends Writer
+{
+    public function process($data)
+    {
+    
+    $document = new DOMDocument();
+	$document->encoding = 'utf-8';
+    $document->xmlVersion = '1.0';
+    $document->formatOutput = true;
+
+	$filename = 'database.xml';
+    $items = $document->createElement('items');
+    
+    
+    for($i = 0; $i < count($data); $i++){
+        $item = $document->createElement('item');
+        foreach($data[$i]->asArray() as $key => $value){
+            $attribute = $document->createElement($key, $value);
+            $item->appendChild($attribute);
+        }
+        $items->appendChild($item);
+    }
+
+    $document->appendChild($items);
+	$document->save($filename);
+    }
+}
+
 $reader = new Reader();
 $reader->read($data);
+$info = $reader->getData();
+$writer = new WriterJSON();
+$writer->process($info);
+$writer = new WriterCSV();
+$writer->process($info);
+$writer = new WriterXML();
+$writer->process($info);
 ?>
